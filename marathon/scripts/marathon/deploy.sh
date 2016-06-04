1561f5a030457b995c2e760bdd74cbb09d89ce0e
Rob Mills <rob.mills@uptake.com>
2016-06-03 22:37:10 -0500

    Add marathon deploy script

---
+++ marathon/scripts/marathon/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/../env
+source ${DIR}/env
+# DO NOT EDIT ABOVE THIS LINE!!!
+
+if [ -n "$1" ]; then
+    curl -H "Content-Type: application/json" -X POST --data @${DIR}/marathon.$1.template.json ${API_ROOT} &&
+else
+    echo "ERROR: Did not specify application to scale! (i.e. api|load-balancer|mesos-dns)"
+fi

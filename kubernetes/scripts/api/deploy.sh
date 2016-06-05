1988ab5ca44e259f06bc7fe97e0aa2b50634ee26
Mariano Gonzalez <mariano.gonzalez@uptake.com>
2016-06-05 08:52:16 -0500

    Fixed docker image tag

--- kubernetes/scripts/api/deploy.sh
+++ kubernetes/scripts/api/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl run ccc-api --image=robgmills/ccc-api:1.0.0 --port=9090
+kubectl run ccc-api --image=robgmills/ccc-api:1.1.0 --port=9090
 kubectl expose rc ccc-api --port=9090 --target-port=9090 --type=NodePort
